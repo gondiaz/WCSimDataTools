d2fd11bccdd53aace304795dbe86eda5f2274b5d
gondiaz <gonzalodiazlopez10@gmail.com>
2024-07-04 15:57:36 +0200

    Fix function logic and delete comented lines

--- src/wcsimreader/utils.py
+++ src/wcsimreader/utils.py
@@ -1,7 +1,7 @@
 import tables as tb
 import pandas as pd
 
-from os.path import expandvars
+from os.path import expandvars, basename
 
 def explore_file(filename):
     filename = expandvars(filename)
@@ -14,17 +14,9 @@ def read_table(filename, path):
     filename = expandvars(filename)
     
     with tb.open_file(filename) as h5f:
+        if not hasattr   (h5f.root, path) : raise LookupError(f"{path} not found in {basename(filename)}")
         table = h5f.root.__getattr__(path)
-        if not hasattr   (h5f.root, path) : raise Exception(":path: not found in :filename:")
-        if not isinstance(table, tb.Table): raise Exception(":path: is not a table in :filename:")
-
-        # if table.nrows == 1:
-        #     sets    = table.read()
-        #     indexes = sets.dtype.fields.keys()
-        #     df = pd.DataFrame(index=indexes, columns=["values"])
-        #     for index in indexes: 
-        #         df.loc[index, "values"] = sets[index][0]
-        # elif table.nrows > 1:
+        if not isinstance(table, tb.Table): raise LookupError(f"{path} is not a table in {basename(filename)}")
         try:
             df = pd.DataFrame.from_records(table.read())
         except ValueError:
